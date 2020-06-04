da4ada3b7d29211adfaa8dba6605304464f077af
jaehee <jaehee@DESKTOP-89PBNPM>
2020-06-04 15:36:19 +0900

    auction domain 구현

--- src/main/java/com/example/jpetstore/domain/Auction.java
+++ src/main/java/com/example/jpetstore/domain/Auction.java
@@ -1,5 +1,56 @@
 package com.example.jpetstore.domain;
 
-public class Auction {
+import java.io.Serializable;
+import java.util.Date;
+
+@SuppressWarnings("serial")
+public class Auction implements Serializable {
+	
+	private int auctionId;
+	private String itemId;
+	private String username;
+	private double biddingPrice;
+	private Date biddingDate;
+	private boolean isSuccessful;
+	
+	
+	public int getAuctionId() {
+		return auctionId;
+	}
+	public void setAuctionId(int auctionId) {
+		this.auctionId = auctionId;
+	}
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public double getBiddingPrice() {
+		return biddingPrice;
+	}
+	public void setBiddingPrice(double biddingPrice) {
+		this.biddingPrice = biddingPrice;
+	}
+	public Date getBiddingDate() {
+		return biddingDate;
+	}
+	public void setBiddingDate(Date biddingDate) {
+		this.biddingDate = biddingDate;
+	}
+	public boolean isSuccessful() {
+		return isSuccessful;
+	}
+	public void setSuccessful(boolean isSuccessful) {
+		this.isSuccessful = isSuccessful;
+	}
+	
+	
 
 }
