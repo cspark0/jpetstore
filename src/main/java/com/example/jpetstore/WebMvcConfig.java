622afecf56bf239f4e3ca3b00f906e61c2e1b4a2
Changsup Park <cspark0@gmail.com>
2021-05-03 17:49:44 +0900

    Use Tiles on Spring Boot

--- src/main/java/com/example/jpetstore/WebMvcConfig.java
+++ src/main/java/com/example/jpetstore/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -28,4 +29,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 					"/shop/viewOrder.do", "/shop/newOrder.do");		
 	}
 	
+	@Override		
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.order(2);		
+	}
 }
