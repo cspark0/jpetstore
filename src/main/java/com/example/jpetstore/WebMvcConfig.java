b7eb673b2981904b5cd3d0f3da2969743a20f700
cspark0 <cspark0@gmail.com>
2021-05-05 18:21:27 +0900

    update using Thymeleaf

--- src/main/java/com/example/jpetstore/WebMvcConfig.java
+++ src/main/java/com/example/jpetstore/WebMvcConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
