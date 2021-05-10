e1ace297132234520779caf254ee69291eff5a25
cspark0 <cspark0@gmail.com>
2021-05-11 01:28:46 +0900

    update PetStoreImpl class

--- src/main/java/com/example/jpetstore/service/PetStoreImpl.java
+++ src/main/java/com/example/jpetstore/service/PetStoreImpl.java
@@ -2,7 +2,6 @@ package com.example.jpetstore.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.jpetstore.dao.AccountDao;
